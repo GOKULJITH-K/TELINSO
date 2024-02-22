7e84f42df03f3add032e51d14dcdb70051b65ccc
GOKULJITH K <87140397+GOKULJITH-K@users.noreply.github.com>
2024-02-23 01:15:06 +0530

    Add files via upload

--- public/soilhealth.js
+++ public/soilhealth.js
@@ -1,9 +1,40 @@
 // This example adds a map using web components.
+function Center(){
+ 
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        
+          const lat =  position.coords.latitude;
+          const lng = position.coords.longitude;
+          
+        const map= document.getElementById("marker-click-event-example");
+        const currentlocation= document.getElementById("current-location");
+        map.setAttribute("center",` ${lat} , ${lng}`);
+        currentlocation.setAttribute("position",`${lat},${lng}`);
+        
+
+        
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      },
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+}
+
+
 function initMap() {
 
+    
 
- 
     console.log("Maps JavaScript API loaded.");
+    
     
     const advancedMarkers = document.querySelectorAll(
       "#marker-click-event-example gmp-advanced-marker",
@@ -28,5 +59,6 @@ function initMap() {
     
   
   }
+  
   
   window.initMap = initMap;
